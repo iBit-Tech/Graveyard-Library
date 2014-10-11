9c653689ada25201f3b704b876c9f8e461d7953b
xbony2 <xbony22@gmail.com>
2014-10-11 09:21:20 -0400

    Added a test class for testing purposes.

---
+++ Graveyard/src/xbony2/test/MainTestClass.java
@@ -0,0 +1,16 @@
+package xbony2.test;
+
+/**
+ * This class is created to test the graveyard
+ * library, it is not an extension of it and should not
+ * be distributed alongside it.
+ * 
+ * @author xbony2
+ *
+ */
+public class MainTestClass {
+
+	public static void main(String[] args){
+		System.out.println("HEYO");
+	}
+}
